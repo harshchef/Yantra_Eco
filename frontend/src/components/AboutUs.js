5a14665ff8c15796366ec90c015f157db2efdacd
Aditya Kumar <adityakumar0718@gmail.com>
2024-04-13 01:25:56 +0530

    Created routes for frontend and basic home and about us Page

---
+++ frontend/src/components/AboutUs.js
@@ -0,0 +1,29 @@
+// AboutUsPage.js
+
+import React from "react";
+import "./AboutUs.css"; // You can create this CSS file for styling
+
+const AboutUsPage = () => {
+  return (
+    <div className="about-us-container">
+      <h1>About Us</h1>
+      <p>
+        Welcome to our company! We are passionate about creating amazing web
+        experiences. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed ac justo euismod, cursus libero vel, fermentum nunc. Nullam nec
+        justo vel libero tincidunt vestibulum. In hac habitasse platea dictumst.
+      </p>
+      <p>
+        Our team consists of talented developers, designers, and dreamers who
+        work together to build innovative solutions. We believe in
+        collaboration, creativity, and continuous learning.
+      </p>
+      <p>
+        Feel free to reach out to us if you have any questions or want to
+        collaborate!
+      </p>
+    </div>
+  );
+};
+
+export default AboutUsPage;
